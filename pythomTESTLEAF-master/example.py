938d90279eb41d56dfa66b5df1219fc22e39b4aa
kytokyko <kaushikswaminathan6@gmail.com>
2020-09-07 16:27:56 +0530

    new chnages in exercise_3 prgm

--- pythomTESTLEAF-master/example.py
+++ pythomTESTLEAF-master/example.py
@@ -1,27 +1,77 @@
-print("welcome to python world")
-a=100
-print(a)
-print(type(a))
-print(id(a))
+# create list
+name=[]
+print(type(name))
+name=['kaushik','venkat','swaminav','babbi']
+print(name,type(name))
 
-a="kaushik"
-print(a)
-print(type(a))
-print(id(a))
+# if we want to get the value from existing index we follow the below
 
-b=100
-print(id(a))
-a=10;b=20;c=30
-print(a,b,c)
-x,y,z=100,200,300
-print(x,y,z)
-a1=b1=c1="value"
-print(a1)
-print(b1)
-print(c1)
-a,b,c=10,12.5,"name"
-print(a,type(a))
-print(b,type(b))
-print(c,type(c))
+print(name[0])
 
+# adding the item in the list
+# -----> append
+# -----> extend
+# -----> insert
+
+# append function ---> will add single item in the list
+name.append("kaushbabbi")
+print(name)
+# o/p ---> ['kaushik', 'venkat', 'swaminav', 'babbi', 'kaushbabbi']
+# it maintains the insertion order
+name.append("venkat")
+print(name)
+# o/p ----> ['kaushik', 'venkat', 'swaminav', 'babbi', 'kaushbabbi', 'venkat']
+# duplicate values are considered in list
+
+# extend function -----> will add multiple items
+ls=['name1','name2','name3']
+name.extend(ls)
+print(name)
+# o/p ----> ['kaushik', 'venkat', 'swaminav', 'babbi', 'kaushbabbi', 'venkat', 'name1', 'name2', 'name3']
+
+# insert function -----> will insert in the required index in the list
+name.insert(3,'special_name')
+name.insert(4,['a','b','c']) # will be considered as one single list in one index
+print(name)
+print(name[3])
+# o/p ----> ['kaushik', 'venkat', 'swaminav', 'special_name', ['a', 'b', 'c'], 'babbi', 'kaushbabbi', 'venkat', 'name1', 'name2', 'name3']
+# o/p ----> special_name
+
+# removing item from the list
+
+# --------> remove()
+# will remove the select element from the list (arg should be the the element)
+mobile=['honor','mi','nokia','samsung']
+mobile.remove('mi')
+print(mobile)
+
+# ---------> pop()
+# pop() will in default remove the last item in the list
+mobile.pop()
+print(mobile)
+
+new=['vivo','oppo','poco','mi']
+mobile.extend(new)
+print(mobile)
+
+# ---------> pop(index)
+# pop(index) will remove the item in the mentioned index
+mobile.pop(3)
+print(mobile)
+
+# ---------> clear()
+# clear() will remove all the items in the list and return an empty list
+mobile.clear()
+print(mobile)
+
+new_list=['poco','mi','samsung','nokia','honor','realme','pixel','vivo','oppo','mi','poco','samsung']
+mobile.extend(new_list)
+
+# ----------> index()
+# index('item') will provide the index in which the item is located
+print(mobile.index('honor'))
+
+# ----------> count(item)
+# count(item)
+print(mobile.count('samsung'))
 
--- testleaf_homeworks/exercise_3.py
+++ testleaf_homeworks/exercise_3.py
@@ -5,16 +5,11 @@
 # Expected Output : (2, 4, 6, 8, 10)
 
 value= (1,2,3,4,5,6,7,8,9,10)
-print(type(value))
-for i in range(1,len(value),2):
-    print(value[i])
 
-# o/p
-#<class 'tuple'>
-#2
-#4
-#6
-#8
-#10
+print(value[1::2])
+
+
+
+
 
 
